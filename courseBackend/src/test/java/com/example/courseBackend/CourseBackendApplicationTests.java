3cab930a8006a83bc144a19346e7d54de606d2c1
itsmeabhishekn <abhishekn4454@gmail.com>
2022-12-06 11:04:49 +0530

    first commit

---
+++ courseBackend/src/test/java/com/example/courseBackend/CourseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
